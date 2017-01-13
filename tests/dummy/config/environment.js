aca68f8f154262eaa61f4cede49a285c21fd6d5d
gczene <gabor@czene.co.uk>
2017-01-13 13:32:23 +0000

    rootUrl of dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/bg-button-group/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
